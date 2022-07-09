ac61ff980a0e844d904912ad482933e61fca511a
YuvarajSingh-0 <yuvarajsingh170@gmail.com>
2022-07-09 19:02:29 +0530

    Minor Changes

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-
-                    "url": f"{url}",
-                    "description": f"**Price** : `{curr_price}`   ~~`{prices[url]}`~~ \n \n **Link** -> [{title}]({url})'",
-                    "color": math.floor(ma
